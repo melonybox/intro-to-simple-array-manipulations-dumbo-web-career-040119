2241f3f4a201deb67ea50e0040980ab3359243e9
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 02:58:56 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array,item)
+  array.push(item)
+end
+
+def using_unshift(array,item)
+  array.unshift(item)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arrayA,arrayB)
+  arrayA.concat(arrayB)
+end
+
+def using_insert(array,item)
+  array.insert(4,item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,item)
+  array.delete(item)
+end
+
+def using_delete_at(array,num)
+  array.delete_at(num)
+end
